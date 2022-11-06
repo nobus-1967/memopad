b7937daa5f74fc793c01e0435760978598add1e4
nobus-1967 <avshcherbina@gmail.com>
2022-11-06 13:56:24 +0300

    Modify `install.sh` for different Linux distros

--- install.sh
+++ install.sh
@@ -3,28 +3,39 @@ clear
 echo "Установка MemoPad:"
 echo "------------------"
 
-export MEMOPAD="$HOME/.local/share/memopad"
-mkdir -vp "$MEMOPAD"
-mkdir -vp "$MEMOPAD/modules"
-mkdir -vp "$MEMOPAD/help"
+export MEMOPAD_DIR="$HOME/.local/share/memopad"
+mkdir -vp "$MEMOPAD_DIR"
+mkdir -vp "$MEMOPAD_DIR/modules" && mkdir -vp "$MEMOPAD_DIR/help"
 
-cp "./modules"/*.py -vu "$MEMOPAD/modules"
-cp "./help"/*.py -vu "$MEMOPAD/help"
-cp ./*.py -vu "$MEMOPAD"
+cp "./modules"/*.py -vu "$MEMOPAD_DIR/modules"
+cp "./help"/*.py -vu "$MEMOPAD_DIR/help"
+cp ./*.py -vu "$MEMOPAD_DIR" 
 
-cd "$MEMOPAD" && echo "$PWD"
+cd "$MEMOPAD_DIR" && echo "$PWD"
 
 echo "ln 'main.py' -> 'memopad'"
-if test -e "$MEMOPAD/memopad"; then
-rm "$MEMOPAD/memopad" && ln main.py memopad
+if test -e "$MEMOPAD_DIR/memopad"; then
+    rm "$MEMOPAD_DIR/memopad" && ln main.py memopad
 else
-ln main.py memopad
+    ln main.py memopad
 fi
 
-echo "PATH -> ~/.bashrc"
-echo '' >> "$HOME/.bashrc"
-echo '# Add MemoPad Path' >> "$HOME/.bashrc"
-echo 'export PATH="$PATH:$HOME/.local/share/memopad"' >> "$HOME/.bashrc"
+if test -e "$HOME/.bash_profile"; then
+    echo '' >> "$HOME/.bash_profile"
+    echo '# Add MemoPad Path' >> "$HOME/.bash_profile"
+    echo 'export PATH="$PATH:$HOME/.local/share/memopad"' >> "$HOME/.bash_profile"
+    echo 'PATH="$PATH:$HOME/.local/share/memopad" -> "$HOME/.bash_profile"' 
+elif test -e "$HOME/.profile"; then
+    echo '' >> "$HOME/.profile"
+    echo '# Add MemoPad Path' >> "$HOME/.profile"
+    echo 'export PATH="$PATH:$HOME/.local/share/memopad"' >> "$HOME/.profile"
+    echo 'PATH="$PATH:$HOME/.local/share/memopad" -> "$HOME/.profile"'
+elif test -e "$HOME/.bashrc"; then    
+    echo '' >> "$HOME/.bashrc"
+    echo '# Add MemoPad Path' >> "$HOME/.bashrc"
+    echo 'export PATH="$PATH:$HOME/.local/share/memopad"' >> "$HOME/.bashrc"
+    echo 'PATH="$PATH:$HOME/.local/share/memopad" -> "$HOME/.bashrc"'
+fi
 
 echo "--------------------"
 echo "Установка завершена."
